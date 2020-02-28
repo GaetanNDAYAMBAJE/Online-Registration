f91011db14f5e6acb5a4671ec7dcd0f628df9a78
Gaetan NDAYAMBAJE <gaetandeo@gmail.com>
2020-02-28 10:19:16 +0200

    My First project

---
+++ function.py
@@ -0,0 +1,22 @@
+#return values
+#def my_function(x):
+   # return 5*x
+#print(my_function(3))
+#print(my_function(5))
+#print(my_function(9))
+#list
+#def sum_list(items):
+    # sum_numbers = 0
+    # for x in items:
+    #    sum_numbers += x
+    #return sum(items)
+#print(sum_list([1,2,8,98]))
+
+#max
+# def max_of_two(x,y):
+#     if x>y:
+#         return x
+#     return y
+# def max_of_three(x,y,z):
+#     return max_of_two(x,max_of_two(y,z))
+# print(max_of_three(4,8,3))
